86cabc468f0cb7143c49ecb136a7cdef2423257e
Gabriel Ruiz <45018237+grmelito@users.noreply.github.com>
2020-10-18 00:43:56 -0300

    Add banner code

--- Port-Scanner.py
+++ Port-Scanner.py
@@ -1,6 +1,10 @@
 import socket,sys
 from datetime import datetime
+import pyfiglet
 
+
+ascii_banner = pyfiglet.figlet_format("Port Scanner")
+print(ascii_banner)
 begin = datetime.now()
 
 topports = [20,21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
